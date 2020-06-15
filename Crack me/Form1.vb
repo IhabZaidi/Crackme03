849a607f96d721532ed4e6b155baf2bc1dbb237f
IhabZaidi <ihab.bibou@gmail.com>
2020-06-15 16:37:12 +0100

    first commit

---
+++ Crack me/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Dim dd As Date = "01/01/2020"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim r As Date = Date.Today
+        If r.ToString("dd/MM/yyyy") > dd Then
+            MsgBox("Trial period has been ended")
+        End If
+    End Sub
+End Class
